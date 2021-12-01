749376ce7b506bc326fd04ccae841677f679ddfa
Tyler Kjeldgaard <kjeldgaard.tyler@gmail.com>
2021-11-30 21:40:06 -0800

    Add util function for parsing direct to numbers.

--- src/util/util.rs
+++ src/util/util.rs
@@ -3,8 +3,12 @@ use std::io;
 use std::io::BufRead;
 use std::path::Path;
 
-// Read file and return array of lines.
 pub fn read_file(path: String) -> Option<Vec<String>> {
+    read_file_str(path)
+}
+
+// Read file and return array of lines.
+pub fn read_file_str(path: String) -> Option<Vec<String>> {
     let os_path = Path::new(&path);
     let file = match File::open(path) {
         Ok(f) => f,
@@ -16,6 +20,23 @@ pub fn read_file(path: String) -> Option<Vec<String>> {
     for line in reader {
         if let Ok(l) = line {
             arr.push(l);
+        }
+    }
+    return Some(arr);
+}
+
+pub fn read_file_nums(path: String) -> Option<Vec<i32>> {
+    let os_path = Path::new(&path);
+    let file = match File::open(path) {
+        Ok(f) => f,
+        Err(err) => return None,
+    };
+    let reader = io::BufReader::new(file).lines();
+    let mut arr: Vec<i32> = Vec::new();
+
+    for line in reader {
+        if let Ok(l) = line {
+            arr.push(l.trim().parse::<i32>().unwrap());
         }
     }
     return Some(arr);
